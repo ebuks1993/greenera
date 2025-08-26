524c2b27ee6822e1477d36504a1c3eb05ff66b9b
Chukwuka Ebuka <chukwuka@Chukwukas-MacBook-Pro-2.local>
2025-08-26 13:20:52 +0100

    first commit

--- fieldsales/admin.py
+++ fieldsales/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 from .models import Appcall , rep,customer,Visit , Appcall,regionManager,areaManager,businessManager,User    
 
@@ -12,4 +14,30 @@ admin.site.register(Appcall)
 admin.site.register(customer)
 admin.site.register(businessManager)
 admin.site.register(User)
+
+
+
+
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ("username", "email", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+    search_fields = ("username", "email")
+    ordering = ("username",)
+
+admin.site.register(User, CustomUserAdmin)
 
